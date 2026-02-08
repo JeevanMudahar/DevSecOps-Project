ed5d342bf39f1d2b21613b1cec15927be80b478b
jeevan <jeevan_mudahar@hotmail.com>
2026-02-08 13:08:22 +0000

    Update

--- main.tf
+++ main.tf
@@ -1,3 +1,15 @@
 provider "aws" {
   region = "eu-west-2" # London region
 }
+resource "aws_s3_bucket" "terraform1137022026" {
+  bucket = "terraform1137022026"
+}
+# Changing location of state file to S3 so that a team would be able to make changes at a single location which is accessible to them.
+terraform {
+  backend "s3" {
+    # Replace this with your bucket name!
+    bucket         = "terraform1137022026"
+    key            = "global/s3/terraform.tfstate"
+    region         = "eu-west-2"
+  }
+}
